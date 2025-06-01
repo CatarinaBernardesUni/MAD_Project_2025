867c2e19594b254fee86c7cb42bb74eb717633ea
CatarinaBernardesUni <20231609@novaims.unl.pt>
2025-06-01 19:23:22 +0100

    firebase

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth'; 
+import { getFirestore } from 'firebase/firestore'; 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGStG2VO6lwvo76Ta3yIawPA6CEkRmyfU",
+  authDomain: "mad-project-2025.firebaseapp.com",
+  projectId: "mad-project-2025",
+  storageBucket: "mad-project-2025.firebasestorage.app",
+  messagingSenderId: "94415246820",
+  appId: "1:94415246820:web:3dc4669735ef16a2f623f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app); 
